f84c50cf174e6a92486589948d0c2112366a1aa4
Yasseeer <153569423+Yasseeer@users.noreply.github.com>
2023-12-20 15:37:30 +0000

    z

--- index.php
+++ index.php
@@ -8,15 +8,6 @@
 <body>
     <h1>Welcome to Your Web App</h1>
 
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web App</title>
-</head>
-<body>
-    <h1>Welcome to Your Web App</h1>
 
     <?php
     $servername = "art.mysql.database.azure.com";
